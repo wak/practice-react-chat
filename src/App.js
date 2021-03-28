0e7882a98246486c3c5705f797f8adbd6da11e29
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2021-03-28 16:11:48 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,94 +1,103 @@
 import React from 'react';
 import './App.css';
 
-class SendMessageForm extends React.Component {
+class ChatControlUI extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
   handleSubmit(event) {
-    if (this.state.value.length == 0)
+    event.preventDefault();
+    if (this.message.value.length === 0)
       return;
 
-    this.props.callback(this.state.value);
-    event.preventDefault();
-    this.setState({ value: '' });
+    this.props.callback(this.message.value);
+    this.message.value = "";
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Message:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Send" />
-      </form>
+      <div className="UI">
+        <form onSubmit={this.handleSubmit}>
+          <table>
+            <tbody>
+              <tr>
+                <td><label>Message:</label></td>
+                <td>
+                  <input
+                    className="MessageTextBox"
+                    type="text"
+                    placeholder="message"
+                    ref={(input) => {this.message = input;}}
+                  />
+                </td>
+                <td><input type="submit" value="Send" /></td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </div>
     );
   }
 }
 
-function ChatEntry(props) {
-  return (
-    <tr>
-      <td>{ props.timestamp }</td>
-      <td>{ props.message }</td>
-    </tr>
-  );
-}
-
 function ChatEntryList(props) {
-  var options = {weekday: 'short', month: 'short', day: 'numeric' };
-  
   const entries =
         props.entries.map(
           (e) =>
-            <ChatEntry
-              timestamp={e.timestamp.toLocaleString()}
-              message={e.message}
-            />
+            <tr>
+              <td>{ e.timestamp.toLocaleString() }</td>
+              <td>{ e.message }</td>
+            </tr>
         );
   return entries;
+}
+
+class ChatBoard extends React.Component {
+  render() {
+    return (   
+      <div className="ChatBoard">
+        <table>
+          <thead>
+            <tr>
+              <th>Timestamp</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            <ChatEntryList entries={this.props.entries}/>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { entries: Array() };
+    this.nexttKey = 1;
+    this.state = { entries: [] };
     this.handleNewMessage = this.handleNewMessage.bind(this);
   }
 
   handleNewMessage(message) {
     var entries = this.state.entries.slice();
-    entries.unshift({ timestamp: new Date(), message: message });
+    entries.unshift({ key: this.nextKey, timestamp: new Date(), message: message });
     this.setState({ entries: entries });
+    this.nextKey += 1;
   }
 
   render() {
     return (
       <div className="App">
         <div className="Title">React Chat</div>
-        <div className="UI">
-          <SendMessageForm callback={this.handleNewMessage}/>
-        </div>
 
-        <div className="ChatBoard">
-          <table>
-            <tr>
-              <th>Timestamp</th>
-              <th>Message</th>
-            </tr>
-            <ChatEntryList entries={this.state.entries}/>
-          </table>
-        </div>
+        <ChatControlUI callback={this.handleNewMessage}/>
+        <ChatBoard entries={this.state.entries} />
       </div>
     );
   }
