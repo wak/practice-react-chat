ff09498b46e03323fe1d961569e708f596622623
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2021-03-29 23:24:09 +0900

    more beautifully

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import moment from 'moment';
 
 var BACKEND_ENTRYPOINT = process.env.REACT_APP_BACKEND_ENDPOINT;
 console.log('backend: ' + BACKEND_ENTRYPOINT);
@@ -13,6 +14,12 @@ const axios = require('axios').create({
   responseType: 'json' 
 });
 
+
+function date_to_timestamp(date) {
+  if (!date)
+    return null;
+  return moment(date).format('YYYY/MM/DD hh:mm:ss');
+}
 
 class ChatControlUI extends React.Component {
   constructor(props) {
@@ -62,8 +69,8 @@ function ChatEntryList(props) {
         props.entries.map(
           (e) =>
             <tr>
-              <td>{ e.timestamp.toLocaleString() }</td>
-              <td>{ e.message }</td>
+              <td className="Timesgamp">{ date_to_timestamp(e.timestamp) }</td>
+              <td className="Message">{ e.message }</td>
             </tr>
         );
   return entries;
@@ -73,11 +80,12 @@ class ChatBoard extends React.Component {
   render() {
     return (   
       <div className="ChatBoard">
+        <p>Last Update: { date_to_timestamp(this.props.lastUpdatedAt) }</p>
         <table>
           <thead>
             <tr>
-              <th>Timestamp</th>
-              <th>Message</th>
+              <th className="Timestamp">Timestamp</th>
+              <th className="Message">Message</th>
             </tr>
           </thead>
           <tbody>
@@ -95,7 +103,9 @@ function WarningBanner(props) {
 
   return (
     <div className="Warning">
-      { props.message }
+      <pre>
+        { props.message }
+      </pre>
     </div>
   );
 }
@@ -121,12 +131,12 @@ class App extends React.Component {
         this.setState({ entries: response.data });
       })
       .catch((error) => {
-        this.setState({ warningMessage: error.toString() });
+        this.setState({ warningMessage: "read message failed: " + error.stack });
         console.log(error);
       })
       .then(() => {
         this.timer = setInterval(this.fetchMessages.bind(this), 10000);
-        this.setState({ lastUpdatedAt: (new Date()).toString() });
+        this.setState({ lastUpdatedAt: (new Date()) });
       });
   }
 
@@ -135,15 +145,13 @@ class App extends React.Component {
       .post('/publish', {
         message: message
       })
-      .then(function (response) {
+      .then((response) => {
         console.log(response);
-      })
-      .catch(function (error) {
-        this.setState({ warningMessage: error.toString() });
-        console.log(error);
-      })
-      .then(() => {
         this.fetchMessages();
+      })
+      .catch((error) => {
+        this.setState({ warningMessage: "send message failed: " + error.stack });
+        console.log(error);
       });
   }
 
@@ -154,11 +162,7 @@ class App extends React.Component {
         <div className="Title">React Chat</div>
 
         <ChatControlUI callback={this.handleNewMessage}/>
-
-        <div>
-          <p>Last Update: {this.state.lastUpdatedAt}</p>
-        </div>
-        <ChatBoard entries={this.state.entries} />
+        <ChatBoard lastUpdatedAt={this.state.lastUpdatedAt} entries={this.state.entries} />
       </div>
     );
   }
